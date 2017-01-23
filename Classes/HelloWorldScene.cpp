9d9fba7c30a79461501587a85794291b3b171247
anhlt570 <anhletuan128@gmail.com>
2017-01-23 11:16:05 +0700

    - Replace sprite frames resources by a sprite sheet - Fit size of frames on animation with the resource image

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -30,28 +30,34 @@ bool HelloWorld::init()
     
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
-	auto helloWorld = Sprite::create("HelloWorld.png", Rect(0,0, 200, 200));
+	auto spriteCache = SpriteFrameCache::getInstance();
+	spriteCache->addSpriteFramesWithFile("my_hello_world.plist");
+
+	auto helloWorld = Sprite::createWithSpriteFrameName("hello_world_01.png");
 	
 	auto moveBy = MoveBy::create(2, Vec2(200, 0));
 	auto rotateBy = RotateBy::create(2, 360);
-	auto scaleBy = ScaleBy::create(2, 1.5f);
+	auto scaleBy = ScaleBy::create(2, 2.5f);
 	auto fadeIn = FadeIn::create(2.0f);
+
+	
+
 	Vector<SpriteFrame*> animFrames;
 	animFrames.reserve(10);
-	animFrames.pushBack(SpriteFrame::create("hello_world_01.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_02.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_03.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_04.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_05.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_06.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_07.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_08.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_09.png", Rect(0,0, 200, 200)));
-	animFrames.pushBack(SpriteFrame::create("hello_world_10.png", Rect(0,0, 200, 200)));
+	animFrames.pushBack(SpriteFrame::create("hello_world_01.png", Rect(0, 0, helloWorld->getContentSize().width, helloWorld->getContentSize().height)));
+	animFrames.pushBack(SpriteFrame::create("hello_world_02.png", Rect(0, 0, helloWorld->getContentSize().width, helloWorld->getContentSize().height)));
+	animFrames.pushBack(SpriteFrame::create("hello_world_03.png", Rect(0, 0, helloWorld->getContentSize().width, helloWorld->getContentSize().height)));
+	animFrames.pushBack(SpriteFrame::create("hello_world_04.png", Rect(0, 0, helloWorld->getContentSize().width, helloWorld->getContentSize().height)));
+	animFrames.pushBack(SpriteFrame::create("hello_world_05.png", Rect(0, 0, helloWorld->getContentSize().width, helloWorld->getContentSize().height)));
+	animFrames.pushBack(SpriteFrame::create("hello_world_06.png", Rect(0, 0, helloWorld->getContentSize().width, helloWorld->getContentSize().height)));
+	animFrames.pushBack(SpriteFrame::create("hello_world_07.png", Rect(0, 0, helloWorld->getContentSize().width, helloWorld->getContentSize().height)));
+	animFrames.pushBack(SpriteFrame::create("hello_world_08.png", Rect(0, 0, helloWorld->getContentSize().width, helloWorld->getContentSize().height)));
 	Animation* myAnimation = Animation::createWithSpriteFrames(animFrames,0.1f);
-	auto frameAnimation = RepeatForever::create(fadeIn);
-	//helloWorld->runAction(Spawn::create(moveBy, rotateBy, scaleBy, frameAnimation, nullptr));
+	auto frameAnimation = RepeatForever::create(Animate::create(myAnimation));
+	auto repeatedFade = RepeatForever::create(fadeIn);
 	helloWorld->runAction(frameAnimation);
+	helloWorld->runAction(Spawn::create(moveBy, rotateBy,  nullptr));
+	
 	helloWorld->setPosition(Vec2(visibleSize.width/2,visibleSize.height/2));
 	this->addChild(helloWorld);
 
